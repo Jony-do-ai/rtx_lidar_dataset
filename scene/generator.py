4ca5c89aaf929c057d6dab1be6c61a5dea04ebb2
Jony-do-ai <fjfjfj215@gamil.com>
2026-02-09 22:53:19 +0800

    阴影二值掩码bug修改

--- scene/generator.py
+++ scene/generator.py
@@ -12,13 +12,11 @@ from omni.isaac.core.objects import GroundPlane, DynamicCuboid, DynamicSphere
 from omni.isaac.core.objects import FixedCuboid
 from omni.isaac.core.utils.prims import delete_prim, is_prim_path_valid
 from omni.isaac.core.utils.semantics import add_update_semantics
-from omni.isaac.sensor import LidarRtx
 # from omni.replicator.core.scripts.utils import get_prim_path
 from pxr import Usd, UsdLux, Sdf
 import omni.usd
 from pxr import UsdLux
 # 自定义模块 (请确保 rtx_lidar_dataset/data/writer.py 存在)
-from file_io.writer import save_pointcloud
 
 
 class SequenceGenerator:
@@ -106,6 +104,8 @@ class SequenceGenerator:
                                                 )
             self.world.reset()
 
+            #光线问题
+            self.set_light_shadows(False)
             # 3. 预热渲染器确保缓存清空
             for _ in range(10):
                 self.world.step(render=True)
@@ -113,6 +113,8 @@ class SequenceGenerator:
             # 4. 抓取图像：这张图就是“纯几何体 + 纯白背景”且无阴影
             rep.orchestrator.step()
             base_rgb = rgb_annotator.get_data()[..., :3].copy()
+            base_rgb_u8 = np.clip(base_rgb, 0, 255).astype(np.uint8)
+            #Image.fromarray(base_rgb_u8).save(os.path.join(geom_dir, "base_no_shadow.png"))
             Image.fromarray(base_rgb).save(os.path.join(geom_dir, "base_no_shadow.png"))
 
             # 5. 结束后关闭环境光，准备生成带阴影的帧
@@ -123,7 +125,7 @@ class SequenceGenerator:
             points = np.random.uniform(-0.25, 0.25, (5000, 3)) + np.array([0, 0, 0.5])
             pcd = o3d.geometry.PointCloud()
             pcd.points = o3d.utility.Vector3dVector(points)
-            o3d.io.write_point_cloud(os.path.join(geom_dir, "gt_fixed.ply"), pcd, write_ascii=True)
+            o3d.io.write_point_cloud(os.path.join(geom_dir, "gt_geometry.ply"), pcd, write_ascii=True)
 
             # 预跑一步
             rep.orchestrator.step()
@@ -142,14 +144,22 @@ class SequenceGenerator:
 
                 rep.orchestrator.step()
                 frame_rgb = rgb_annotator.get_data()[..., :3].copy()
+                frame_rgb_u8 = np.clip(frame_rgb, 0, 255).astype(np.uint8)
+                # Image.fromarray(frame_rgb_u8).save(os.path.join(frame_dir, "rgb_with_shadow.png"))
                 Image.fromarray(frame_rgb).save(os.path.join(frame_dir, "rgb_with_shadow.png"))
-
                 # --- 5. 差分与掩码提取 ---
+                # diff = (base_rgb - frame_rgb)  # 不要 abspath
+                # diff_gray = diff.mean(axis=2)
+                # diff_img = diff.mean(axis=2).astype(np.uint8)
+                # max_diff = diff_gray.max()
+                # thr = max(5.0, 0.15 * max_diff)
+                # shadow_mask = (diff_gray > thr).astype(np.uint8) * 255
+
                 diff = np.abs(base_rgb.astype(np.float32) - frame_rgb.astype(np.float32))
                 diff_img = diff.mean(axis=2).astype(np.uint8)
                 shadow_mask = (diff_img > 20).astype(np.uint8) * 255  # 阈值化提取阴影
 
-                Image.fromarray(diff_img).save(os.path.join(frame_dir, "diff_visual.png"))
+
                 Image.fromarray(shadow_mask).save(os.path.join(frame_dir, "shadow_mask.png"))
 
                 # 保存光线参数供训练使用
